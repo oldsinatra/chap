ee58b1c339a4fe6d2b3e594c94b9c0f0f2cc1bb9
= <=>
2020-07-01 19:02:09 -0700

    Prepopulate users and questions tables using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {   
     protected $fillable = ['title','body'];
+    
     public function user(){
-        return $this->belongsTo(User::class);    }
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title']= $value;
+        $this->attributes['slug']= str_slug($value);
+    }
 }
